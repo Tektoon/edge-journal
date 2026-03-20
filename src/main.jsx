d4ca0999ab40bd26c226eaf9fdd0cfd8ac00439e
Guillaume <g.pasquereau59@outlook.fr>
2026-03-20 19:19:31 +0100

    ajout PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Enregistrement du Service Worker (PWA)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(() => console.log('SW enregistré ✅'))
+      .catch((err) => console.log('SW erreur:', err));
+  });
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
